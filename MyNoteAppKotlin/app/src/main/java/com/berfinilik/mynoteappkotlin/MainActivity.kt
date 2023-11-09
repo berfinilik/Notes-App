7df58722287e16625712e631a85103c7f0c29e71
Berfin İlik <berfinnilik1074@gmail.com>
2023-11-09 11:27:35 +0300

    Notes App
    Bu proje, kullanıcıların kişisel notlar almasına ve düzenlemesine olanak tanır. Basit ve kullanıcı dostu bir arayüze sahiptir.

---
+++ MyNoteAppKotlin/app/src/main/java/com/berfinilik/mynoteappkotlin/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.berfinilik.mynoteappkotlin
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+
+
+
+}
---
+++ MyNoteAppKotlin/app/src/main/java/com/berfinilik/mynoteappkotlin/mvvm/NotesViewModel.kt
@@ -0,0 +1,88 @@
+package com.berfinilik.mynoteappkotlin.mvvm
+
+import android.app.Application
+import android.database.Observable
+import android.util.Log
+import androidx.lifecycle.*
+import androidx.navigation.NavController
+import com.berfinilik.mynoteappkotlin.db.Notes
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NotesViewModel(private val repository: NotesRepository) : ViewModel() {
+
+
+    val inputTitle = MutableLiveData<String>()
+    val inputNoteText = MutableLiveData<String>()
+    val inputSubtitle = MutableLiveData<String>()
+
+    val displayAllnotes = repository.allnotes()
+
+    fun addNotes(){
+
+        val title  = inputTitle.value
+        val subtitle = inputSubtitle.value
+        val text = inputNoteText.value
+        insertNote(Notes(0, title!!, subtitle!!, text!!))
+
+
+    }
+
+    fun insertNote(note: Notes) = viewModelScope.launch(Dispatchers.IO){
+        repository.insertNote(note)
+
+
+    }
+
+
+
+    fun editupdateNote(note: Notes) = viewModelScope.launch(Dispatchers.IO){
+        repository.updateNotes(note)
+    }
+
+
+
+
+    fun editUpdate(id: Int, title: String, sub: String, text: String ){
+
+        editupdateNote(Notes(id, title, sub, text))
+
+        Log.e("MYVIEWMODEL", sub)
+        Log.e("MYVIEWMODEL", id.toString())
+        Log.e("MYVIEWMODEL", title)
+        Log.e("MYVIEWMODEL", sub)
+
+
+
+
+    }
+
+    fun getValuesforUpdating(notes: Notes)  {
+
+
+
+
+
+
+
+    }
+
+
+    fun delete(notes: Notes) = viewModelScope.launch(Dispatchers.IO) {
+        repository.deleteNote(notes)
+    }
+
+    fun deleteOneNote(id: Int, title: String, sub: String, text: String){
+
+        delete(Notes(id, title, sub, text))
+
+    }
+
+
+    fun clearAll() = viewModelScope.launch(Dispatchers.IO) {
+        repository.deleteAll()
+    }
+
+
+}
